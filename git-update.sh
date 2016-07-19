14de1137bfb1ca0482b7dc9387c2cbeab3b9c95d
mirek001 <mirek.grzegorczyk@gmail.com>
2016-07-19 10:03:22 +0200

    \34  \34

--- git-update.sh
+++ git-update.sh
@@ -2,6 +2,6 @@
 git add .
 echo -n "New comment:"
 read $comment
-git commit -m \"$comment\"
+git commit -m  "\34 $comment \34"
 git remote add origin https://github.com/mirek001/smc.git
 git push -u origin master
