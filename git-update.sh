5f374ce57685c7c7dbd94633b442a35a4954290e
mirek001 <mirek.grzegorczyk@gmail.com>
2016-07-19 11:37:32 +0200

    1.2.201

--- git-update.sh
+++ git-update.sh
@@ -2,6 +2,6 @@
 git add .
 echo -n "New comment:"
 read $comment
-git commit -m  "\x022 $comment \x022"
+git commit -m  "1.2.201"
 git remote add origin https://github.com/mirek001/smc.git
 git push -u origin master
